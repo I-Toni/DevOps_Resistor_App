adefb3a550dfff1d24f3c1a0b4d769ec07afb790
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-07-15 02:28:10 -0400

    added test methods to test getResistanceAndTolerance method returns accurate resistance when multiplier is .01

--- src/app/services/resistance.service.spec.ts
+++ src/app/services/resistance.service.spec.ts
@@ -113,4 +113,13 @@ describe('ResistanceService', () => {
     resistanceService.setTolerance("1%");
     expect(resistanceService.getResistanceAndTolerance()).toEqual('32.1 +/- 1%');
   });
+  
+  it('should have getResistanceAndTolerance method return 32.1 +/- 1% when multiplier is .01', () => {
+    resistanceService.setFirstDigit("3");
+    resistanceService.setSecondDigit("2");
+    resistanceService.setThirdDigit("1");
+    resistanceService.setMultipier("x.01");
+    resistanceService.setTolerance("1%");
+    expect(resistanceService.getResistanceAndTolerance()).toEqual('3.21 +/- 1%');
+  });
 });
