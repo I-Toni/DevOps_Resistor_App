a919acd0e6274fba10f7ae873361ad9acde28d5a
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-07-16 15:43:23 -0400

    added tolerance test file to test tolerance object is created

---
+++ src/app/models/tolerances.spec.ts
@@ -0,0 +1,8 @@
+import { Tolerances } from './tolerances';
+
+describe('Tolerances model that stores array of tolerances', () => {
+  it('should create an instance of tolerances', () => {
+    let tolerances = new Tolerances();
+    expect(tolerances).toBeTruthy();
+  });
+});
