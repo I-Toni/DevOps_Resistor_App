3511fa53931e748d8cbf1dd3e9c9af7d4908072b
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-07-16 14:41:55 -0400

    added test method to test that multipliers object is created

---
+++ src/app/models/multipliers.spec.ts
@@ -0,0 +1,9 @@
+import { Multipliers } from './multipliers';
+
+describe('Multipliers model that stores array of multipliers', () => {
+  it('should create an instance of multipliers', () => {
+    let multipliers = new Multipliers();
+    expect(multipliers).toBeTruthy();
+  });
+
+});
