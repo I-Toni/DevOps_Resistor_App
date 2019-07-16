c108dbf5a9fc08054bfb75597e50efb409d815de
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-07-16 16:01:19 -0400

    added test method to test getTolerance method returns array of tolerance

--- src/app/models/tolerances.spec.ts
+++ src/app/models/tolerances.spec.ts
@@ -5,4 +5,9 @@ describe('Tolerances model that stores array of tolerances', () => {
     let tolerances = new Tolerances();
     expect(tolerances).toBeTruthy();
   });
+  
+  it('should have values in tolerances array', () => {
+    let tolerances = new Tolerances();
+    expect(tolerances.getTolerances()).toEqual(['1%', '2%', '0.5%', '0.25%', '0.1%', '0.05%', '5%', '10%', '20%']);
+  });
 });
