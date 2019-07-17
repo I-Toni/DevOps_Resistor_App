6f4fb7ee0151ce73a3a70592e1a7774cbb89039a
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-07-17 16:22:30 -0400

    initialized instance variables for selected options with first array values and changed ngValue to value in html

--- src/app/resistor/resistor-calculator/resistor-calculator.component.ts
+++ src/app/resistor/resistor-calculator/resistor-calculator.component.ts
@@ -13,6 +13,11 @@ export class ResistorCalculatorComponent implements OnInit {
   public digitsList: string[];
   public multiplierList: string[];
   public toleranceList: string[];
+  public firstDigit: string;
+  public secondDigit: string;
+  public thirdDigit: string;
+  public digitsMultiplier: string;
+  public digitsTolerance: string;
   
   constructor(private resistanceService: ResistanceService) {}
 
@@ -24,6 +29,13 @@ export class ResistorCalculatorComponent implements OnInit {
     this.multiplierList = multiplier.getMultipliers();
     let tolerance = new Tolerances();
     this.toleranceList = tolerance.getTolerances();
+    
+    this.firstDigit = this.digitsList[0];
+    this.secondDigit = this.digitsList[0];
+    this.thirdDigit = this.digitsList[0];
+    this.digitsMultiplier = this.multiplierList[0];
+    this.digitsTolerance = this.toleranceList[0];
   }
+ 
 
 }
